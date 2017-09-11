b5072d187791beb3da02dad1a61b9b8a636da17b
emily <limeyb7@gmail.com>
2017-09-11 14:00:44 +0100

    Install linter, fix existing linting errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const gulp = require("gulp");
-const pug = require("gulp-pug");
-const rename = require("gulp-rename");
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const rename = require('gulp-rename')
 
-const renamePug = path => Object.assign({}, path, { extname: ".html" });
+const renamePug = path => Object.assign({}, path, { extname: '.html', })
 
-gulp.task("pug", () =>
+gulp.task('pug', () =>
   gulp
-    .src("src/templates/views/*.pug")
-    .pipe(pug({ pretty: true }))
+    .src('src/templates/views/*.pug')
+    .pipe(pug({ pretty: true, }))
     .pipe(rename(path => renamePug(path)))
-    .pipe(gulp.dest("public"))
-);
+    .pipe(gulp.dest('public'))
+)
