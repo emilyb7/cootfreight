ad3aeb1e3889782c871ec7fc0cea56c700533d12
emily <limeyb7@gmail.com>
2017-09-11 13:31:36 +0100

    adds basic file structure using gulp and pug

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const rename = require("gulp-rename");
+
+const renamePug = path => Object.assign({}, path, { extname: ".html" });
+
+gulp.task("pug", () =>
+  gulp
+    .src("src/templates/views/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(rename(path => renamePug(path)))
+    .pipe(gulp.dest("public"))
+);
