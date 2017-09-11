3c8409c6fd49db5b6d348b2349442d6636bcdf76
emily <limeyb7@gmail.com>
2017-09-11 15:38:04 +0100

    amends gulp script to watch css and pug files resolves #2

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const pug = require('gulp-pug')
 const rename = require('gulp-rename')
 const cleanCSS = require('gulp-clean-css')
 const concatCss = require('gulp-concat-css')
+const browserSync = require('browser-sync').create()
 
 const renamePug = path => Object.assign({}, path, { extname: '.html', })
 
@@ -12,6 +13,11 @@ gulp.task('pug', () =>
     .pipe(pug({ pretty: true, }))
     .pipe(rename(path => renamePug(path)))
     .pipe(gulp.dest('public'))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    )
 )
 
 gulp.task('css', () =>
@@ -20,4 +26,22 @@ gulp.task('css', () =>
     .pipe(concatCss('main.css'))
     .pipe(cleanCSS({ compatibility: 'ie8', }))
     .pipe(gulp.dest('public/css'))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    )
 )
+
+gulp.task('browsersync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'public',
+    },
+  })
+})
+
+gulp.task('watch', [ 'browsersync', 'pug', 'css', ], function() {
+  gulp.watch('src/templates/**/*.pug', [ 'pug', ])
+  gulp.watch('src/css/*.css', [ 'css', ])
+})
