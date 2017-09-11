ca040af89dc1fbc72bb9b5c1d5c055c93d928103
emily <limeyb7@gmail.com>
2017-09-11 14:52:27 +0100

    adds gulp script for concatenating and minifying css related #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp')
 const pug = require('gulp-pug')
 const rename = require('gulp-rename')
+const cleanCSS = require('gulp-clean-css')
+const concatCss = require('gulp-concat-css')
 
 const renamePug = path => Object.assign({}, path, { extname: '.html', })
 
@@ -10,4 +12,12 @@ gulp.task('pug', () =>
     .pipe(pug({ pretty: true, }))
     .pipe(rename(path => renamePug(path)))
     .pipe(gulp.dest('public'))
+)
+
+gulp.task('css', () =>
+  gulp
+    .src([ 'src/css/*.css', ])
+    .pipe(concatCss('main.css'))
+    .pipe(cleanCSS({ compatibility: 'ie8', }))
+    .pipe(gulp.dest('public/css'))
 )
