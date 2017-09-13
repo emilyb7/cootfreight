95e6cabf56e9b5adf2b093c2b38d6ef857e339e5
emily <limeyb7@gmail.com>
2017-09-13 17:12:57 +0100

    content for air freight page related #36

---
+++ src/content/markdown/air-freight.md
@@ -0,0 +1,6 @@
+* Worldwide, general air freight
+* Door to door deliveries
+* Project cargo
+* Consolidation
+* Cargo insurance
+* Sea-to-air and air-to-land
---
+++ src/content/views/air-freight.js
@@ -0,0 +1,8 @@
+const { fileToHtml, } = require('../../utils.js')
+
+module.exports = {
+  view: 'air-freight',
+  pageTitle: 'Air freight | Coot Freight Ltd',
+  headline: 'Air freight',
+  body: fileToHtml('air-freight.md'),
+}
