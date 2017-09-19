8049087d0fcffd8340841ad1ef12b9bde3c70df9
emily <limeyb7@gmail.com>
2017-09-19 12:31:16 +0100

    add file with container dimensions related #48

--- src/content/views/sea-freight.js
+++ src/content/views/sea-freight.js
@@ -6,5 +6,7 @@ module.exports = {
   headline: 'Sea freight',
   body1: fileToHtml('sea-freight_1.md'),
   body2: fileToHtml('sea-freight_2.md'),
+  tableHeading: 'Container dimensions',
+  table: require('../container_dimensions.json'),
   cta: 'Get a quote!',
 }
